2817349ea59cd701b090671a2ce7ac4a9ff5770b
kuanyilee85 <kuanyilee85@gmail.com>
2021-06-11 16:16:45 -0400

    Joining more than Two Tables

---
+++ Section 7 Selecting From Multiple Tables /Joining more than Two Tables.sql
@@ -0,0 +1,16 @@
+USE coffee_store;
+
+SELECT * FROM products;
+SELECT * FROM customers;
+SELECT * FROM orders;
+
+-- inner join more than two tables, products as table 1
+SELECT p.name,p.price,c.first_name,c.last_name,o.order_time FROM products p
+JOIN orders o ON p.id = o.product_id
+JOIN customers c ON c.id = o.customer_id;
+
+SELECT p.name,p.price,c.first_name,c.last_name,o.order_time FROM products p
+JOIN orders o ON p.id = o.product_id
+JOIN customers c ON c.id = o.customer_id
+WHERE last_name = 'Martin'
+ORDER BY o.order_time;
