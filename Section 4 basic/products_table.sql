75109522de16b22113b02893f955db153e812e72
kuanyilee85 <kuanyilee85@gmail.com>
2021-06-11 13:44:39 -0400

    INSERT products, orders, customers table

---
+++ Section 4 basic/products_table.sql
@@ -0,0 +1,3 @@
+INSERT INTO products (name,price,coffee_origin) VALUES ('espresso',2.50,'brazil'),('macchiato',3.00,'brazil'),('cappuccino',3.50,'costa rica'),('latte',3.50,'indonesia'),('americano',3.00,'brazil'),('flat white',3.50,'indonesia'),('fliter',3.00,'india');
+
+SELECT * FROM products;
