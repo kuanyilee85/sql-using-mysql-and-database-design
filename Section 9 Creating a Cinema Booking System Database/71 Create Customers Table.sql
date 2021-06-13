7d40103236fca4d3e1f2d214f3d06168b6d93aa2
kuanyilee85 <kuanyilee85@gmail.com>
2021-06-13 09:21:41 -0400

    CREATE customers TABLE

---
+++ Section 9 Creating a Cinema Booking System Database/71 Create Customers Table.sql
@@ -0,0 +1,15 @@
+ SHOW DATABASES;
+  
+ USE cinema_booking_system;
+ 
+ CREATE TABLE customers (
+ 
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(45),
+    last_name VARCHAR(45) NOT NULL,
+    email VARCHAR(45) NOT NULL UNIQUE
+ );
+ 
+ SHOW TABLES;
+ 
+ DESCRIBE customers;
