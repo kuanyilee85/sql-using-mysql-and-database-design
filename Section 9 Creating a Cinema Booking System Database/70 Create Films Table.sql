c6cb18f194d87e124f7bcfcd5e2ac86adf5de543
kuanyilee85 <kuanyilee85@gmail.com>
2021-06-13 09:08:17 -0400

    CREATE cinema DATABASE, CREATE films TABLE

---
+++ Section 9 Creating a Cinema Booking System Database/70 Create Films Table.sql
@@ -0,0 +1,18 @@
+ SHOW DATABASES;
+ 
+ CREATE DATABASE cinema_booking_system;
+ 
+ USE cinema_booking_system;
+ 
+ CREATE TABLE films (
+	
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(45) NOT NULL UNIQUE,
+    length_name INT NOT NULL
+ );
+ 
+ SHOW TABLES;
+ 
+ SELECT * FROM films;
+ 
+ DESCRIBE films;
