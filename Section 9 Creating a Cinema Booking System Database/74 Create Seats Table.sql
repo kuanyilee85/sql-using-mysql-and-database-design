f67db44ce7441e46c904f7fb28b0110c2bad1208
kuanyilee85 <kuanyilee85@gmail.com>
2021-06-13 09:52:59 -0400

    CREATE seats TABLE

---
+++ Section 9 Creating a Cinema Booking System Database/74 Create Seats Table.sql
@@ -0,0 +1,16 @@
+ SHOW DATABASES;
+  
+ USE cinema_booking_system;
+ 
+ CREATE TABLE seats (
+ 
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    seatrow CHAR(1) NOT NULL,
+    number INT NOT NULL,
+    room_id INT NOT NULL,
+    FOREIGN KEY (room_id) REFERENCES rooms(id)
+ );
+ 
+ SHOW TABLES;
+ 
+ DESCRIBE seats;
